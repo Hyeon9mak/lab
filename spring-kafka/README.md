5f7618b5245c95911849338d2eab13491da83a30
Hyeon9mak <jinha3507@gmail.com>
2025-12-14 12:08:38 +0900

    spring-kafka: Kraft 모드를 이용한 controller, broker 다중화

---
+++ spring-kafka/README.md
@@ -0,0 +1,3 @@
+# spring-kafka
+
+- `docker-compose.yml` 를 이용해 local kafka broker 실행 (9092 port) 후 테스트 가능
--- spring-kafka/src/main/kotlin/com/hyeon9mak/springkafka/KafkaConfig.kt
+++ spring-kafka/src/main/kotlin/com/hyeon9mak/springkafka/KafkaConfig.kt
@@ -1,12 +1,24 @@
 package com.hyeon9mak.springkafka
 
+import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.TopicBuilder
+import org.springframework.kafka.core.KafkaAdmin
 
 @Configuration
 class KafkaConfig {
+
+    /**
+     * KafkaAdmin Bean 설정
+     * 별도 옵션을 설정하고 싶다면 이를 통해 진행하면 됨
+     * 생략 가능
+     */
+    @Bean
+    fun admin(): KafkaAdmin = KafkaAdmin(mapOf(
+        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:19092,localhost:39092,localhost:49092"),
+    )
 
     /**
      * Topic 자동 생성 설정
@@ -14,8 +26,8 @@ class KafkaConfig {
      * (단, kafka 2.4 이상 버전부터 유효한 방식)
      */
     @Bean
-    fun topic1(): NewTopic = TopicBuilder.name("thing1")
-        .partitions(2)
+    fun kafkaStudyTopic(): NewTopic = TopicBuilder.name("kafka-study-topic")
+        .partitions(3)
         .replicas(3)
         .compact()
         .build()
